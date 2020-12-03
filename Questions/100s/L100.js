ac7ab5d9b58edcd108a35c16bb666b2c4a268d8e
fengcheng0126 <fengcheng0126@gmail.com>
2020-12-03 10:30:57 -0600

    ais8dguo8c7wtnr

---
+++ Questions/100s/L100.js
@@ -0,0 +1,11 @@
+function answer1() {
+    document.getElementById("question").innerHTML="Wrong. The correct answer is 1:6";
+}
+
+function answer2() {
+    document.getElementById("question").innerHTML="Wrong. The correct answer is 1:6";
+}
+
+function answer3() {
+    document.getElementById("question").innerHTML="Ding ding ding!!!";
+}
