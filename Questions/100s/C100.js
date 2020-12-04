4b4b9c075b64abe2d4fc0a8450ebeed8d891253d
fengcheng0126 <fengcheng0126@gmail.com>
2020-12-04 15:16:44 -0600

    lauwgelnuwe

--- Questions/100s/C100.js
+++ Questions/100s/C100.js
@@ -1,2 +1,13 @@
 "use strict";
 
+function answer1() {
+    document.getElementById("question").innerHTML="Wrong. The right answer is 7:43";
+}
+
+function answer2() {
+    document.getElementById("question").innerHTML="Ding ding ding!!!";
+}
+
+function answer3() {
+    document.getElementById("question").innerHTML="Wrong. The right answer is 7:43";
+}
