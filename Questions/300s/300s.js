f5c391fe82c23cebf911f6262adcdaa801f6e97c
fengcheng0126 <fengcheng0126@gmail.com>
2020-12-06 20:50:57 -0600

    a;osuifh;puwegpf

---
+++ Questions/300s/300s.js
@@ -0,0 +1,17 @@
+"use strict";
+
+function Lanswer1() {
+    document.getElementById("question").innerHTML="Wrong. The correct answer is 2:5:15";
+}
+
+function Lanswer2() {
+    document.getElementById("question").innerHTML="Wrong. The correct answer is 2:5:15";
+}
+
+function Lanswer3() {
+    document.getElementById("question").innerHTML="Ding ding ding!!!";
+}
+
+function Lanswer4() {
+    document.getElementById("question").innerHTML="Wrong. The correct answer is 2:5:15";
+}
