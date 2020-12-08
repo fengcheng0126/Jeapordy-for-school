0badd43e401b1426b583ce1fb8f1d8f54efe80b8
fengcheng0126 <fengcheng0126@gmail.com>
2020-12-08 11:19:01 -0600

    o9asuhfo9a78gwof87ge

--- Questions/300s/300s.js
+++ Questions/300s/300s.js
@@ -3,15 +3,25 @@
 function Lanswer1() {
     document.getElementById("question").innerHTML="Wrong. The correct answer is 2:5:15";
 }
-
 function Lanswer2() {
     document.getElementById("question").innerHTML="Wrong. The correct answer is 2:5:15";
 }
-
 function Lanswer3() {
     document.getElementById("question").innerHTML="Ding ding ding!!!";
 }
-
 function Lanswer4() {
     document.getElementById("question").innerHTML="Wrong. The correct answer is 2:5:15";
+}
+
+function Ranswer1() {
+    document.getElementById("question").innerHTML="Wrong. The correct answer is 2:5:2:55";
+}
+function Ranswer2() {
+    document.getElementById("question").innerHTML="Wrong. The correct answer is 2:5:2:55";
+}
+function Ranswer3() {
+    document.getElementById("question").innerHTML="Wrong. The correct answer is 2:5:2:55";
+}
+function Ranswer4() {
+    document.getElementById("question").innerHTML="Ding ding ding!!!";
 }
