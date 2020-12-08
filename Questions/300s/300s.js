c64a52cdc61e6be53ddf5ebf60576f6f13712a43
fengcheng0126 <fengcheng0126@gmail.com>
2020-12-08 11:47:58 -0600

    ;aosuhfloiuhfpaodf

--- Questions/300s/300s.js
+++ Questions/300s/300s.js
@@ -25,3 +25,16 @@ function Ranswer3() {
 function Ranswer4() {
     document.getElementById("question").innerHTML="Ding ding ding!!!";
 }
+
+function Canswer1() {
+    document.getElementById("question").innerHTML="Wrong. The correct answer is 6:4:1:1";
+}
+function Canswer2() {
+    document.getElementById("question").innerHTML="Ding ding ding!!!";
+}
+function Canswer3() {
+    document.getElementById("question").innerHTML="Wrong. The correct answer is 6:4:1:1";
+}
+function Canswer4() {
+    document.getElementById("question").innerHTML="Wrong. The correct answer is 6:4:1:1";
+}
---
+++ index.php
@@ -0,0 +1,1 @@
+<?php header('Location: /andrew/Jeapordy-for-school'); ?>
