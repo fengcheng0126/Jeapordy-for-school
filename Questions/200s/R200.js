2627cecbd73151ade678401490e1fbf0c6887aa9
fengcheng0126 <fengcheng0126@gmail.com>
2020-12-13 16:13:29 -0600

    alisudhpf9a8whf

--- Questions/200s/R200.js
+++ Questions/200s/R200.js
@@ -5,13 +5,13 @@ function answer1() {
 }
 
 function answer2() {
-    document.getElementById("question").innerHTML="Wrong. The correct answer is 5:1";
+    document.getElementById("question").innerHTML="Wrong. The correct answer is 4:5";
 }
 
 function answer3() {
-    document.getElementById("question").innerHTML="Wrong. The correct answer is 5:1";
+    document.getElementById("question").innerHTML="Wrong. The correct answer is 4:5";
 }
 
 function answer4() {
-    document.getElementById("question").innerHTML="Wrong. The correct answer is 5:1";
+    document.getElementById("question").innerHTML="Wrong. The correct answer is 4:5";
 }
